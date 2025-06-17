09426e4ff001590b08ce747a12cddf7242634b32
Soham Kaul <115170081+Soham11k@users.noreply.github.com>
2025-06-17 16:20:37 +0530

    Update train_digit_cnn.py

--- train_digit_cnn.py
+++ train_digit_cnn.py
@@ -1,27 +1,31 @@
-import tensorflow as tf
+from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
 
+# Load MNIST
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+# Preprocess
 x_train = x_train.reshape(-1, 28, 28, 1).astype("float32") / 255.0
 x_test = x_test.reshape(-1, 28, 28, 1).astype("float32") / 255.0
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
 
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
-
+# Model
 model = Sequential([
     Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
-    MaxPooling2D((2,2)),
+    MaxPooling2D(2,2),
     Conv2D(64, (3,3), activation='relu'),
-    MaxPooling2D((2,2)),
+    MaxPooling2D(2,2),
     Flatten(),
-    Dense(100, activation='relu'),
+    Dense(128, activation='relu'),
     Dense(10, activation='softmax')
 ])
 
+# Compile & Train
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=5)
-model.save("digit_classifier.h5")
+model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
+
+# Save
+model.save("digit_model.h5")
